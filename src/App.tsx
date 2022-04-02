b149814f6580323a6e23da5fa9eaf52ed0056292
Suresh <SBandi@scgcontractor.com>
2022-04-03 00:33:12 +0530

    Updated routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Home } from './components/pages/Home/Home.component';
 import { SingleBlog } from './components/Blogs/Blogs.SingleBlog';
 
@@ -7,7 +7,8 @@ import { SingleBlog } from './components/Blogs/Blogs.SingleBlog';
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
+      <Redirect from='/' to='/home' />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/singleBlog" component={SingleBlog} />
     </BrowserRouter>
   );
