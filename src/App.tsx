0e508fd07ffefdd7184ca9fe839584e53630f894
Suresh <SBandi@scgcontractor.com>
2022-04-03 08:25:15 +0530

    Added image reponsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Home } from './components/pages/Home/Home.component';
-import { SingleBlog } from './components/Blogs/Blogs.SingleBlog';
+import { BlogDetail } from './components/Blogs/BlogDetail.component';
 
 
 export const App: React.FC = () => {
@@ -9,7 +9,7 @@ export const App: React.FC = () => {
     <BrowserRouter>
       <Redirect from='/' to='/home' />
       <Route exact path="/home" component={Home} />
-      <Route exact path="/singleBlog" component={SingleBlog} />
+      <Route exact path="/singleBlog" component={BlogDetail} />
     </BrowserRouter>
   );
 };
--- src/components/Blogs/BlogDetail.component.tsx
+++ src/components/Blogs/BlogDetail.component.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './Blogs.module.scss';
 import { Grid, Label } from 'semantic-ui-react';
 import { Layout } from '../Layout/Layout.component';
-export const SingleBlog: React.FC = () => {
+
+export const BlogDetail: React.FC = () => {
     return (
         <Layout>
             <div className="sectionPadding">
@@ -11,9 +12,9 @@ export const SingleBlog: React.FC = () => {
                         <Grid.Column mobile={16} tablet={8}>
                             <div className={styles['singleBlog']}>
                                 <p><time>October 4, 2021</time>
-                                    <Label >Blockchain</Label></p><h1>A primer on blockchain and its value beyond cryptocurrencies</h1>
+                                    <Label className='tinyMarginLeft'>Blockchain</Label></p><h1>A primer on blockchain and its value beyond cryptocurrencies</h1>
                                 <p>Nowadays, it appears as if everyone is buying cryptocurrencies and Non-Fungible Tokens (NFTs). Big tech and even governments have joined too, but a blockchain is more than trading tokens on an exchange.</p>
-                                <img alt="" src="https://images.prismic.io/byont-website/109c66b1-ca61-44a9-afda-8cff7603954b_04.png?auto=compress,format&amp;rect=0,0,1280,720&amp;w=512&amp;h=288" sizes="100vw" decoding="async" width="1200" height="700"></img>
+                                <img alt="detail1" src="https://images.prismic.io/byont-website/109c66b1-ca61-44a9-afda-8cff7603954b_04.png?auto=compress,format&amp;rect=0,0,1280,720&amp;w=512&amp;h=288" />
                                 <div>
                                     <p>After private investors started noticing cryptocurrencies after the initial boom of Bitcoin in 2013, many new cryptos appeared as altcoins (<strong>alt</strong>ernative&nbsp;<strong>coin</strong>s), attracting more private investors. A few years later, institutional traders joined the market too, and now even governments are buying these digital assets.</p>
                                     <p>Adoption by governments began with El Salvador accepting Bitcoin as a legal tender last year, which compelled many other countries to seriously consider making similar moves, including Paraguay, Brazil, Mexico, Panama, and more. But why is everyone trying to pick the fruits of this new technology? And what can you do with it?</p>
@@ -26,7 +27,7 @@ export const SingleBlog: React.FC = () => {
                                     <h2>Popular use-cases</h2>
                                     <p>Powered by these smart contracts, the most popular use case of blockchain technology is decentralized finance (Defi), which is an umbrella term for financial applications leveraging cryptocurrencies. Defi includes applications for lending and borrowing, community investments, crypto savings, stable coins, and much more. These projects usually live on the Ethereum, Polygon, or Binance blockchain, but many more chains exist.</p>
                                     <p>To give an idea of the size of Defi: at the end of the previous year, the total value locked in public Defi projects was USD 250 billion, compared to only USD 1 billion one and a half years prior in May 2020. The total crypto market numbers are even more impressive, reaching nearly&nbsp;<a target="_blank" rel="noopener noreferrer" href="https://coinmarketcap.com/">16,000</a>&nbsp;unique cryptocurrencies and a total market capitalization of around USD 2 trillion, about two-thirds of Apple. The number of exchanges that facilitate trade in these coins has already surpassed&nbsp;<a target="_blank" rel="noopener noreferrer" href="https://coinmarketcap.com/">440</a>.</p>
-                                    <img alt="Top 25 most funded industries in blockchain / crypto according to BlockData." src="https://images.prismic.io/byont-website/dd9d0e03-d005-436e-ab78-7c240e1ebc84_top-25-most-funded-industries-in-blockchain-crypto.webp?auto=compress,format&amp;rect=1,0,1919,1279&amp;w=1200&amp;h=800" sizes="100vw" decoding="async" width="1200" height="800" ></img>
+                                    <img alt="Top 25 most funded industries in blockchain / crypto according to BlockData." src="https://images.prismic.io/byont-website/dd9d0e03-d005-436e-ab78-7c240e1ebc84_top-25-most-funded-industries-in-blockchain-crypto.webp?auto=compress,format&amp;rect=1,0,1919,1279&amp;w=1200&amp;h=800" />
                                     <p>In a future article, we will go more in-depth about other interesting use-cases, like Non-Fungible Tokens (NFTs) and more.</p>
                                     <h2>Public versus Private blockchains</h2>
                                     <p>Like Bitcoin’s network, the public blockchains mentioned before are open for anyone to join and participate in the consensus protocol, provided they meet the essential qualification standards such as having considerable computational power. All data on these blockchains are public, making them a good fit for upcoming Decentralized Autonomous Organizations or DAOs in short (read our article on what they are). However, being public also makes them a big red target for hackers and enthusiasts looking for privacy and security vulnerabilities.</p>
