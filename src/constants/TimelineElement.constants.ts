3bb8187c3cb673572926bf42d3e7c372b25eeb79
Praveen Pujar <pujarpraveen9@gmail.com>
2022-04-03 13:26:25 +0530

    Update TimelineElement.constants.ts
    Changed the timeline constant

--- src/constants/TimelineElement.constants.ts
+++ src/constants/TimelineElement.constants.ts
@@ -1,60 +1,68 @@
 export const TimelineElements = [
     {
         id: 1,
-        title: "Website Launch",
+        title: "Socialize Website : Q1 2022",
         subTitle: "Lorem Ipsum is simply",
         description:
-            "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
+            "Socialize is one stop platform for all the blockchain solutions.",
         buttonText: "View Frontend Projects",
         date: "Phase 1 - present",
         icon: "check",
     },
     {
         id: 2,
-        title: "A Crypto SWAP Platform",
+        title: "Socialize Learn : Q2 2022",
         subTitle: "Lorem Ipsum is simply",
-        description:
-            "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
+        list : [
+            { key: 1, value: 'socialize NFT marketplace' },
+            { key: 2, value: 'socialize Token and Whitepaper' },
+            ],
         buttonText: "View Backend Projects",
         date: "Phase 2",
         icon: "tasks",
     },
     {
         id: 3,
-        title: "Socialize Metaverse",
+        title: "Socialize Play : Q3 2022",
         subTitle: "Lorem Ipsum is simply",
-        description:
-            "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
+        list : [
+            { key: 1, value: 'socialize Music' },
+            { key: 2, value: 'socialize Video' },
+            { key: 3, value: 'socialize Games' },
+            ],
         buttonText: "Company Website",
         date: "Phase 3",
         icon: "tasks",
     },
     {
         id: 4,
-        title: "Socialize Solutions",
+        title: "Socialize Earn : Q4 2022",
         subTitle: "Lorem Ipsum is simply",
-        description:
-            "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+        list : [
+            { key: 1, value: 'socialize Wallet' },
+            { key: 2, value: 'socialize Uniswap' },
+            { key: 3, value: 'socialize Exchange' }
+            ],
         buttonText: "Course Certificate",
         date: "Phase 4",
         icon: "tasks",
     },
     {
         id: 5,
-        title: "Socialize Exchange, Wallet",
+        title: "Socialize Blockchain : Q1 2023",
         subTitle: "Lorem Ipsum is simply",
         description:
-            "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+        "Blockchain and its solutions",
         buttonText: "College Projects",
         date: "Phase 5",
         icon: "tasks",
     },
     {
         id: 6,
-        title: "Socialize Blockchain, Games, Music",
+        title: "Socialize Metaverse : Q2 2023",
         subTitle: "Lorem Ipsum is simply",
         description:
-            "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+            "A virtual world of endless possibilities.",
         date: "Phase 6",
         icon: "tasks",
     },
