0a0186c5eac678889e4dd71b79f2641dbdbddc45
Suresh <SBandi@scgcontractor.com>
2022-04-05 11:02:14 +0530

    Footer section changes

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -15,46 +15,21 @@ export const Footer: React.FC = () => {
             <div className="wrapper">
                 <Grid>
                     <Grid.Row>
-                        <Grid.Column computer={4} mobile={16}>
+                        <Grid.Column computer={8} tablet={8} mobile={16}>
                             <div className={'logoText'}>
                                 <p>Socialize</p>
                                 <div className={styles["logoTextSec"]}>
                                     <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis non, fugit totam vel laboriosam vitae.
+                                        Learn, Play & Earn
                                     </p>
                                 </div>
                             </div>
 
                         </Grid.Column>
-                        <Grid.Column computer={4} mobile={16} className={styles['links']}>
-                            <div>
-                                <Header as='h3' content='Useful Links' />
-                                <List className={styles['navLinks']}>
-                                    <List.Item><a>All NFTs</a></List.Item>
-                                    <List.Item><a>How It Works</a></List.Item>
-                                    <List.Item><a>Create</a></List.Item>
-                                    <List.Item><a>Explore</a></List.Item>
-                                    <List.Item><a>Privacy & Terms</a></List.Item>
-                                </List>
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column computer={4} mobile={16} className={styles['links']}>
-                            <div>
-                                <Header as='h3' content='Community' />
-                                <List className={styles['navLinks']}>
-                                    <List.Item><a>Help Center</a></List.Item>
-                                    <List.Item><a>Partners</a></List.Item>
-                                    <List.Item><a>Suggestions</a></List.Item>
-                                    <List.Item><a>Blog</a></List.Item>
-                                    <List.Item><a>Newsletter</a></List.Item>
-                                </List>
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column computer={4} mobile={16} className={styles['links']}>
+                        <Grid.Column computer={8} tablet={8} mobile={16} className={[styles['links'], styles['rightAlign']].join(' ')}>
                             <div>
                                 <Header as='h3' content='Contact Us' />
                                 <List className={styles['navLinks']}>
-                                    <List.Item><a href="tel:+91-9988774455">+91-9988774455</a></List.Item>
                                     <List.Item><a href='mailto: support@socialize.com'>support@socialize.com</a></List.Item>
                                 </List>
                                 <div className={styles['socialShare']}>
