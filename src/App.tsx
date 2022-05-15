7f67f96cfb3216dcdd80d2b605677b133b0b31fb
Suresh <SBandi@scgcontractor.com>
2022-05-15 21:37:21 +0530

    Added reactapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './components/pages/Home/Home.component';
 import { BlogDetail } from './components/Blogs/BlogDetail.component';
 
@@ -7,8 +7,7 @@ import { BlogDetail } from './components/Blogs/BlogDetail.component';
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Redirect from='/' to='/home' />
-      <Route exact path="/home" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/singleBlog" component={BlogDetail} />
     </BrowserRouter>
   );
